0dffebc81b170c36be3aa23e44bcb3c2ad12327f
Aryansh Tripathi <tripathi.aryansh.2002@gmail.com>
2024-05-31 00:23:12 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { StreamVideoProvider } from "@/providers/StreamClientProvider";
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+	title: "Yoom",
+	description: "A full stack video conferencing app developed using NextJS",
+	icons: {
+		icon: "/icons/logo.svg",
+	},
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+	return (
+		<main>
+			<StreamVideoProvider>{children}</StreamVideoProvider>
+		</main>
+	);
+};
+
+export default RootLayout;
